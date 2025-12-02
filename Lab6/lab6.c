baf647769c56dd77d6c0f24ded49d5eb071e9c52
PS49638DOVANTHANG <do07611@gmail.com>
2025-12-02 13:05:34 +0700

    Update lab6.c

--- Lab6/lab6.c
+++ Lab6/lab6.c
@@ -1,93 +1,85 @@
 #include <stdio.h>
-//_________________________________________BAI 1______________________________________________________
-void tbtongchiahetcho3() {
-    int n;
-    printf("Nhap vao so phan tu:");
-    scanf("%d", &n);
-    int mang[n];
-    // Nhập vào phần tử
-    for (int i = 0; i < n; i++) {
-        printf("Nhap vao phan tu thu %d:", i + 1);
-        scanf("%d", &mang[i]);
-    }
-    float tong = 0;
-    float biendem = 0; 
-    for (int i = 0; i < n; i++) {
-        if (mang[i] % 3 == 0) {
-            tong += mang[i];
-            biendem++;
+#define M 50
+#define N 50
+// định nghĩa hàm
+void nhapmang2chieu(int a[M][N], int m, int n);
+void xuatmang2chieu(int a[M][N], int m, int n);
+void xuatchiahet3(int a[M][N],int m, int n);
+void giatrilonnhat(int a[M][N], int m, int n);
+void giatrinhonhat(int a[M][N], int m, int n);
+
+//trien khai
+void nhapmang2chieu(int a[M][N], int m, int n){
+    for (int i = 0; i < m; i++)
+    {   
+        for (int j = 0; j < n; j++)
+        {
+            printf("Nhap phan tu a[%d][%d]: ",i,j);
+            scanf("%d", &a[i][j]); 
         }
-    }
-    // Kiểm tra bd > 0 trước khi chia để tránh lỗi chia cho 0
-    if (biendem > 0) {
-        printf("trung binh tong cac so chia het cho 3 trong mang la:%.2f\n", tong / biendem);
-    } else {
-        printf("Khong co so nao chia het cho 3 trong mang.\n");
     }
 }
-//___________________________________________BAI 2_________________________________________________________
-void timmaxmintrongmang(){
-    int n;
-    printf("Nhap vao so phan tu:");
-    scanf("%d",&n);
-    int mang[n];
-    //nhap vao phan tu
-    for (int i = 0; i < n; i++){
-        printf("Nhap vao phan tu thu %d:",i+1);
-        scanf("%d",&mang[i]);
-    }
-    //max
-    int tempmax=mang[0];
-    for (int i = 0; i < n; i++){
-        if (mang[i]>tempmax){
-            tempmax=mang[i];
+
+void xuatmang2chieu(int a[M][N], int m, int n){
+     printf("Xuat mang a[%d][%d]: \n",m,n);
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ",a[i][j]);
         }
+        printf("\n");
     }
-    //min
-    int tempmin=mang[0];
-    for (int i = 0; i < n; i++){
-        if (mang[i]<tempmin){
-            tempmin=mang[i];
-        }
-    }
-    printf("max la:%d\n",tempmax);
-    printf("min la:%d\n",tempmin);
 }
-//___________________________________________BAI 3_____________________________________________________________
-void sapxepmanggiamdan() {
-    int n;
-    printf("Nhap vao so phan tu:");
-    scanf("%d", &n);
-    int mang[n];
-    // Nhập vào phần tử
-    for (int i = 0; i < n; i++) {
-        printf("Nhap vao phan tu thu %d:", i + 1);
-        scanf("%d", &mang[i]);
+
+void xuatchiahet3(int a[M][N],int m, int n){
+    printf("Xuat phan tu chia het cho 3: ");
+    for(int i = 0; i<m; i++){
+        for (int j=0; j<n; j++)
+        {
+            if(a[i][j] % 3 == 0){
+                printf("%d ", a[i][j]);
+            }
+        }
+        
     }
-    // Thuật toán Sắp xếp Nổi bọt (Bubble Sort) giảm dần
-    int temp;
-    for (int i = 0; i < n-1 ; i++) { // Vòng lặp bên ngoài
-        for (int j = 0; j < n -1 - i; j++) { // Vòng lặp bên trong
-            // Nếu phần tử hiện tại nhỏ hơn phần tử kế tiếp thì hoán vị 
-            if (mang[j] < mang[j + 1]) { 
-                temp = mang[j];
-                mang[j] = mang[j + 1];
-                mang[j + 1] = temp;
+}
+
+void giatrilonnhat(int a[M][N], int m, int n){
+    int max = a[0][0]; 
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            if(max < a[i][j]){
+                max = a[i][j];
             }
         }
     }
-
-    printf("mang da sap xep giam dan la:");
-    for (int i = 0; i < n; i++) {
-        printf("%d,", mang[i]);
-    }
-    printf("\n");
+    printf("\nMax = %d", max);
 }
 
-int main() {
-    tbtongchiahetcho3();
-    timmaxmintrongmang();
-    sapxepmanggiamdan();
-    return 0;
+void giatrinhonhat(int a[M][N], int m, int n){
+    int min = a[0][0];
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            if(min > a[i][j]){
+                Min = a[i][j];
+            }
+        }
+    }
+    printf("\nMin = %d", min);
+}
 
+
+int main(){
+    // nhap mang 2 chieu
+    int a[M][N], m, n;
+    printf("Nhap m va n: ");
+    scanf("%d %d", &m,&n);
+    nhapmang2chieu(a,m,n);
+    xuatmang2chieu(a,m,n);
+    xuatchiahet3(a,m,n);
+    giatrilonnhat(a,m,n);
+    giatrinhonhat(a,m,n);
+
+    return 0;
 }
